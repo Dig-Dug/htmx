17a11a15d0405a32ee829da4e42c44088d1d0cf2
unknown <40063087+Dig-Dug@users.noreply.github.com>
2023-08-01 21:23:49 +0200

    serverListenPostWorking

--- bxp/demoDb.js
+++ bxp/demoDb.js
@@ -1,23 +1,30 @@
-/* let mysql = require("mysql");
-let con = mysql.createConnection({
-  host: "localhost",
-  user: "felipeDB",
-  password: "321a",
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use(express.static("public"));
+app.get("/index.html", function (req, res) {
+  res.sendFile(__dirname + "/" + "index.htm");
 });
-con.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
-  con.query("CREATE DATABASE mydb", function (err, result) {
-    console.log("DB :D");
-  });
-}); */
-let express = require("express");
-let app = express();
-app.get("/", function (req, res) {
-  res.send("HElkll--23245");
+
+app.post("/process_post", urlencodedParser, function (req, res) {
+  // Prepare output in JSON format
+  response = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+  };
+  console.log(response);
+  res.end(JSON.stringify(response));
 });
-let server = app.listen(8081, function () {
-  let host = server.address().address;
-  let port = server.address().port;
-  console.log("Example at http://%s:%s", host, port);
+
+var server = app.listen(8081, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("Example app listening at http://%s:%s", host, port);
 });
+
+//https://www.tutorialspoint.com/nodejs/nodejs_express_framework.htm#
